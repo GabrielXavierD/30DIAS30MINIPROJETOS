23a0c6991bcae1bafbd9d37de4d9212d7d650973
GabrielXavierD <gabrielxavierdominguess@gmail.com>
2024-04-06 14:34:37 -0300

    .

---
+++ DIA 22/script.js
@@ -0,0 +1,11 @@
+function fundo(){
+    let fundo = document.querySelector("body").style.backgroundColor;
+
+if(fundo ==  "white"){
+    document.querySelector("h2").style.color = "white";
+     document.querySelector("body").style.backgroundColor = "#333333";
+  
+ }else{
+   
+ }
+    }
